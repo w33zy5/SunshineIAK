8bcea0493d8922ac9461064ed1c192762e48dd6c
I Dewa Gede Agung Krisnayana <dewaagung44@gmail.com>
2017-08-13 14:36:46 +0800

    Create layout detail and detail activity

---
+++ app/src/main/java/com/example/satyakresna/sunshineiak/DetailActivity.java
@@ -0,0 +1,17 @@
+package com.example.satyakresna.sunshineiak;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Dewa Agung on 13/08/17.
+ */
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
